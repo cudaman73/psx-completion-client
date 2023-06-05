d12f17e4fdafa16d3ddcd085330d2094c94cd253
cudaman73 <dale.evans.3@gmail.com>
2023-06-05 12:43:11 -0500

    adds lazy loading for images

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
     return (
         <div className="card text-bg-primary mb-3">
             <h5 className="card-header">{props.name}</h5>
-            <img src={props.url} className="card-img-top pt-2 px-2" alt="game cover thumbnail" />
+            <img loading="lazy" src={props.url} className="card-img-top pt-2 px-2" alt="game cover thumbnail" />
             <div className="card-body">
                 {/* <p className="card-text">{props.completed == true ?  "Uncomplete" : "Complete"}</p> */}
             </div>
